8e2724713d329e8c33dd40f546c60f32a04f75b7
Profetto Marco <profetto.marco@gmail.com>
2023-12-16 16:22:25 +0100

    nan, inf, -inf, inff, -inff symbols added

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: mprofett <mprofett@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 13:11:11 by mprofett          #+#    #+#             */
-/*   Updated: 2023/12/07 10:06:02 by mprofett         ###   ########.fr       */
+/*   Updated: 2023/12/16 16:20:36 by mprofett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,34 +19,33 @@ bool	nbr_is_valid(std::string nbr)
 	it = nbr.begin();
 	while (*it == ' ' && it != nbr.end())
 		++it;
-	if (it == nbr.end() || isdigit(*it) == 0)
-		return (0);
-	if (isdigit(*it) == 1 || *it == '+' || *it == '-')
-		++it;
-	while (isdigit(*it) == 1 && it != nbr.end())
+	if (it == nbr.end() || (isdigit(*it) == 0 && !(*it == '+' || *it == '-')))
+		return (false);
+	++it;
+	while (isdigit(*it) != 0 && it != nbr.end())
 		++it;
 	if (it == nbr.end())
-		return (1);
+		return (true);
 	if (*it == 'f')
 	{
 		++it;
 		if (it != nbr.end())
-			return (0);
+			return (false);
 		else
-			return (1);
+			return (true);
 	}
 	if (*it != '.')
-		return (0);
+		return (false);
 	++it;
-	while (isdigit(*it) == 1 && it != nbr.end())
+	while (isdigit(*it) != 0 && it != nbr.end())
 		++it;
 	if (it == nbr.end())
-		return (1);
+		return (true);
 	if (*it == 'f')
 		++it;
 	if (it == nbr.end())
-		return (1);
-	return (0);
+		return (true);
+	return (false);
 }
 
 bool	nbr_is_zero(std::string nbr)
@@ -56,32 +55,31 @@ bool	nbr_is_zero(std::string nbr)
 	it = nbr.begin();
 	while (*it == ' ' && it != nbr.end())
 		++it;
-	if (it == nbr.end() || *it != '0')
-		return (0);
-	if (*it == '0' || *it == '+' || *it == '-')
-		++it;
-	while (*it == '0' && it != nbr.end())
-		++it;
-	if (it == nbr.end())
-		return (1);
-	if (*it == 'f')
-	{
-		++it;
-		if (it != nbr.end())
-			return (0);
-	}
-	if (*it != '.')
-		return (0);
+	if (it == nbr.end() || (isdigit(*it) == 0 && !(*it == '+' || *it == '-')))
+		return (false);
 	++it;
 	while (*it == '0' && it != nbr.end())
 		++it;
 	if (it == nbr.end())
-		return (1);
+		return (true);
+	if (*it == 'f')
+	{
+		++it;
+		if (it != nbr.end())
+			return (false);
+	}
+	if (*it != '.')
+		return (false);
+	++it;
+	while (*it == '0' && it != nbr.end())
+		++it;
+	if (it == nbr.end())
+		return (true);
 	if (*it == 'f')
 		++it;
 	if (it == nbr.end())
-		return (1);
-	return (0);
+		return (true);
+	return (false);
 }
 
 bool	is_displayable(int	nbr)
@@ -91,9 +89,23 @@ bool	is_displayable(int	nbr)
 	return (1);
 }
 
+bool	int_convertion_failed(long int nbr, std::string str)
+{
+	if (nbr == 0 && nbr_is_zero(str) == 0)
+		return (true);
+	return (false);
+}
+
+bool	double_convertion_failed(double nbr, std::string str)
+{
+	if (nbr == 0 && nbr_is_zero(str) == 0)
+		return (true);
+	return (false);
+}
+
 bool	is_char(long int nbr)
 {
-	if (nbr > CHAR_MAX || nbr < CHAR_MIN)
+	if (nbr > CHAR_MAX || nbr < 0)
 		return (0);
 	return (1);
 }
@@ -107,19 +119,30 @@ bool	is_integer(long int nbr)
 
 bool	is_float(double nbr)
 {
-	if ((nbr > FLT_MAX && nbr != INFINITY) || (nbr < FLT_MIN && nbr != -INFINITY))
-		return (0);
-	return (1);
+	if ((nbr > FLT_MAX && nbr != INFINITY) || (nbr < -FLT_MAX && nbr != -INFINITY))
+		return (false);
+	return (true);
 }
 
-void	display_char(long int nbr)
+bool	nbr_is_special(std::string str)
+{
+	if (str.compare("nan") == 0 || str.compare("nanf") == 0
+		|| str.compare("inf") == 0 || str.compare("-inf") == 0
+		|| str.compare("inff") == 0 || str.compare("-inff") == 0)
+		return (true);
+	return (false);
+}
+
+
+void	display_char(long int nbr, std::string str)
 {
 	char	c;
 
-	if (is_displayable(nbr) == 0)
-		std::cout << "char: Non displayable\n";
-	else if (is_char(nbr) == 0)
+
+	if (is_char(nbr) == 0 || int_convertion_failed(nbr, str) == true || nbr_is_special(str) == true)
 		std::cout << "char: impossible\n";
+	else if (is_displayable(nbr) == 0)
+		std::cout << "char: Non displayable\n";
 	else
 	{
 		c = static_cast<char>(nbr);
@@ -127,11 +150,11 @@ void	display_char(long int nbr)
 	}
 }
 
-void	display_int(long int nbr)
+void	display_int(long int nbr, std::string str)
 {
 	int	i;
 
-	if (is_integer(nbr) == 0)
+	if (is_integer(nbr) == 0 || int_convertion_failed(nbr, str) == true || nbr_is_special(str) == true)
 		std::cout << "int: impossible\n";
 	else
 	{
@@ -140,11 +163,11 @@ void	display_int(long int nbr)
 	}
 }
 
-void	display_float(double nbr)
+void	display_float(double nbr, std::string str)
 {
 	float	f;
 
-	if (is_float(nbr) == 0)
+	if ((is_float(nbr) == 0 || double_convertion_failed(nbr, str) == true) && nbr_is_special(str) == false)
 		std::cout << "float: impossible\n";
 	else
 	{
@@ -156,12 +179,40 @@ void	display_float(double nbr)
 	}
 }
 
-void	display_double(double nbr)
+void	display_double(double nbr, std::string str)
 {
-	std::cout << "double: " << nbr;
-	if (nbr - static_cast<long int>(nbr) == 0)
-		std::cout << ".0";
-	std::cout << std::endl;
+	if (double_convertion_failed(nbr, str) == true && nbr_is_special(str) == false)
+		std::cout << "double: impossible\n";
+	else
+	{
+		std::cout << "double: " << nbr;
+		if (nbr - static_cast<long int>(nbr) == 0)
+			std::cout << ".0";
+		std::cout << std::endl;
+	}
+}
+
+
+bool	get_special_nbr(std::string str, long int *int_nbr, double *double_nbr)
+{
+	if (str.compare("nan") == 0 || str.compare("nanf") == 0)
+	{
+		*int_nbr = NAN;
+		*double_nbr = NAN;
+	}
+	else if (str.compare("inf") == 0 || str.compare("inff") == 0)
+	{
+		*int_nbr = INFINITY;
+		*double_nbr = INFINITY;
+	}
+	else if (str.compare("-inf") == 0 || str.compare("-inff") == 0)
+	{
+		*int_nbr = -INFINITY;
+		*double_nbr = -INFINITY;
+	}
+	else
+		return (false);
+	return (true);
 }
 
 void	ScalarConverter::convert(std::string str)
@@ -169,26 +220,20 @@ void	ScalarConverter::convert(std::string str)
 	long int	int_nbr;
 	double		double_nbr;
 
-	if (nbr_is_valid(str) == 0)
+	if (nbr_is_special(str) == true)
+		get_special_nbr(str, &int_nbr, &double_nbr);
+	else if (nbr_is_valid(str) == true)
+	{
+		int_nbr = strtol(str.c_str(), NULL, 10);
+		double_nbr = strtod(str.c_str(), NULL);
+	}
+	else
 	{
 		std::cout << "char: impossible\nint: impossible\nfloat: impossible\ndouble: impossible\n";
 		return;
 	}
-	int_nbr = strtol(str.c_str(), NULL, 10);
-	if (int_nbr == 0 && nbr_is_zero(str) == 0)
-		std::cout << "char: impossible\nint: impossible\n";
-	else
-	{
-		display_char(int_nbr);
-		display_int(int_nbr);
-	}
-	double_nbr = strtod(str.c_str(), NULL);
-	if (double_nbr == 0 && nbr_is_zero(str) == 0)
-		std::cout << "float: impossible\ndouble: impossible\n";
-	else
-	{
-		display_float(double_nbr);
-		display_double(double_nbr);
-	}
-
+	display_char(int_nbr, str);
+	display_int(int_nbr, str);
+	display_float(double_nbr, str);
+	display_double(double_nbr, str);
 }
